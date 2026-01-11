0c99ca7f7310a0fd508f03795c4efcc6d98c5890
luna <lucassilvavg9@gmail.com>
2026-01-11 12:24:29 -0300

    Objeto para novas despesas criado.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
+const form = document.querySelector("form")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () =>{
@@ -16,5 +19,18 @@ function formatCurrencyBRL(value){
     })
 
     return value
+}
+
+form.onsubmit = (event) =>{
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        created_at: new Date(),
+    }
 }
 
